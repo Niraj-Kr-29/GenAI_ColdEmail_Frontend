9240a8fd35e411f1f5c6738d798d6159f6d72060
Niraj-Kr-29 <nkr2348@gmail.com>
2025-07-06 13:59:13 +0530

    css

--- src/components/KeyFeatures.jsx
+++ src/components/KeyFeatures.jsx
@@ -31,10 +31,10 @@ const KeyFeatures = () => {
           Key Features
           <img className="w-12" src={featureIcon} />
         </div>
-        <div className="flex gap-14 overflow-x-auto overflow-visible h-[26rem] w-full">
+        <div className="flex gap-14 overflow-x-auto overflow-visible h-[26rem] w-full lg:justify-center">
           {
             keyFeatures.map((feature,index)=>{
-              return <div className="sm:w-72 lg:w-60 h-[20rem] mt-6"><FeatureCard key={index} title={feature.title} description={feature.description}/></div>
+              return <div className="sm:w-72 lg:w-56 h-[20rem] mt-6"><FeatureCard key={index} title={feature.title} description={feature.description}/></div>
             })
           }
         </div>
